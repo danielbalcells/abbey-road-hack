4f969c01c4c8c2043aa3a28d104dca066d543a57
Daniel Balcells <dbalcells@gmail.com>
2018-11-10 23:55:12 +0000

    Fuck genre

--- backend/models.py
+++ backend/models.py
@@ -13,14 +13,3 @@ class Track(models.Model):
     instrumentalness = models.FloatField()
     energy = models.FloatField()
     year = models.IntegerField()
-
-
-class TrackGenres(models.Model):
-
-    track = models.ForeignKey('Track', on_delete=models.CASCADE)
-    genre = models.ForeignKey('Genre', on_delete=models.CASCADE)
-
-
-class Genre(models.Model):
-
-    name = models.CharField(max_length=500)
