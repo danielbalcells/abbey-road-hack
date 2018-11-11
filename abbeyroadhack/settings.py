3e1b8f6d51784065a437e3062a038a2314083d4a
Enric Calabuig <ecalabuig@bmat.com>
2018-11-11 11:03:59 +0000

    Add rest_framework to settings

--- abbeyroadhack/settings.py
+++ abbeyroadhack/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'backend',
-    'django_extensions'
+    'django_extensions',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
